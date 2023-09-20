c680711beb7497bd7bae39617aa30907fc350aa9
Rodrigo Perdomo <97471272+xrodrigopx@users.noreply.github.com>
2023-09-20 17:22:29 -0300

    replace
    lab2

--- laboratorio2/laboratorio2/Lab2_interuppciones.asm
+++ laboratorio2/laboratorio2/Lab2_interuppciones.asm
@@ -5,11 +5,13 @@
 ; Author : curso de microprocesadores
 ;
 
-
+PCINT10
 
 ; Empiezo con los vectores de interrupción
 .ORG 0x0000
-	jmp		start		;dirección de comienzo (vector de reset)  
+	jmp		start		;dirección de comienzo (vector de reset)
+.org	0x0008 
+	jmp		interrupcion_externa 
 .ORG 0x001C 
 	jmp		_tmr0_int	;salto atención a rutina de comparación A del timer 0
 
@@ -33,24 +35,41 @@ start:
 	ldi		r16,	0b00000101	
 	out		TCCR0B,	r16			;prescaler = 1024
 	ldi		r16,	124	
-	out		OCR0A,	r16			;comparo con 125
+	out		OCR0A,	r16			;comparo con 124
 	ldi		r16,	0b00000010	
 	sts		TIMSK0,	r16			;habilito la interrupción (falta habilitar global)
 ;-------------------------------------------------------------------------------------
 ;Inicializo algunos registros que voy a usar como variables.
 	ldi		r24,	0x00		;inicializo r24 para un contador genérico
 ;-------------------------------------------------------------------------------------
+ldi		r16, 0b00000010
+sts		PCICR,r16
+ldi		r16, 0b00000100
+sts		PCMSK1,r16
+
 
 
 ;Programa principal ... acá puedo hacer lo que quiero
 
 comienzo:
-	sei		;habilito las interrupciones globales(set interrupt flag) en SREG
-	sleep
+	sei							;habilito las interrupciones globales(set interrupt flag)
 
 loop1:
-rjmp loop1
-
+	nop
+	nop
+	nop
+	nop
+	ori r16, 0xFF
+	nop
+	nop
+	nop
+	brne	loop1  //branch not equal 0 la zero flag es 0 salta si la operacion anterior da cero por ende va a loop 2
+loop2:
+	nop
+	nop
+	nop
+fin:
+	rjmp loop2
 
 ;RUTINAS
 ;-------------------------------------------------------------------------------------
@@ -65,19 +84,31 @@ rjmp loop1
 ; Esta rutina por ahora no hace casi nada, Ud puede ir agregando funcionalidades.
 ; Por ahora solo: cambia el valor de un LED en la placa, e incrementa un contador en r24.
 
-_tmr0_int:	
-		in		r16, SREG; guardar el contexto cuando se entra en la interrupción, SREG = CONTEXTO que va al r16.
-		; AHORA HACEMOS LA INTERRUPCION YA CON EL CONTEXTO GUARDADO
-L0:		inc		r24	 ;cuento cuántas veces entré en la rutina.	
-L1:
-		cpi		r24, 125	
-L2:
-		brne	L1		
-		sbi		PINB,	3			;toggle LED
+_tmr0_int:
+		in	    r17,SREG 			
+		inc		r24	
+		cpi		r24,125
+		breq	cambio_led	
+		rjmp	_tmr0_out
 
-		; PRONTA LA INTERRUPCIÓN, TENEMOS QUE RECUPERAR EL CONTEXTO QUE HABIA EN SREG
-		out		SREG, r16; out = recupera lo que estaba en r16, lo guarda en SREG con los valores originales, porque SREG se modificó durante la interrupción
-
+cambio_led:	
+			sbi		PINB,	2			;cambio LED
 
 _tmr0_out:
+		out		SREG,r17
+			   
 	    reti						;retorno de la rutina de interrupción del Timer0
+
+
+
+
+interrupcion_externa:
+					
+					in	    r17,SREG 
+								
+					sbi		PINB, 5	
+												;toggle LED camvia de estado D1
+					inc		r24					;cuento cuántas veces entré en la rutina.
+
+					out		SREG,r17
+					reti		
---
+++ laboratorio2/laboratorio2/interrupciones/main1.asm
@@ -0,0 +1,163 @@
+;
+; lab2.asm
+;
+; Created: 19/9
+; Author : rperdomo
+;
+
+
+
+; Empiezo con los vectores de interrupción
+.ORG 0x0000 ;rutina estándar
+	jmp		start		;dirección de comienzo (vector de reset)
+.org	0x0008 ;rutina de interrupcion externa
+	jmp		interrupcion_externa 
+.ORG 0x001C ;rutina del timer
+	jmp		_tmr0_int	;salto atención a rutina de comparación A del timer 0
+
+
+; ---------------------------------------------------------------------------------------
+; acá empieza el programa
+start:
+;configuro los puertos:
+;	PB2 PB3 PB4 PB5	- son los LEDs del shield
+    ldi	r16,	0b00111101	;edefino los led que voy a usar
+	out	DDRB,	r16			;los defino como salida
+	out	PORTB,	r16			;apago los LEDs
+
+	ldi	r16,	0b00000000	
+	out	DDRC,	r16			;3 botones del shield son entradas
+;-------------------------------------------------------------------------------------
+
+;Configuro el TMR0 y su interrupcion.
+	ldi	r16,	0b00000010	
+	out	TCCR0A,	r16			;configuro para que cuente hasta OCR0A y vuelve a cero (reset on compare), ahí dispara la interrupción
+	ldi	r16,	0b00000101	;
+	out	TCCR0B,	r16			;prescaler = 1024
+	ldi	r16,	124			;
+	out	OCR0A,	r16			;comparo con 124, cuando esto llega a 1024 interrumpe, entonces el led hace lo que le pedimos, pero no va a hacer nada si no habilitamos las int globales
+	ldi	r16,	0b00000010	;
+	sts	TIMSK0,	r16			;habilito la interrupción (falta habilitar global)
+;-------------------------------------------------------------------------------------
+;Inicializo algunos registros que voy a usar como variables.
+	ldi	r24,	0x00		;inicializo r24 para un contador genérico
+	ldi	r18,	0;mi registro de incrementos
+	ldi	r19,	0;el registro del segundo bit
+	ldi	r20,	0;el registro del tercer bit
+;-------------------------------------------------------------------------------------
+	ldi	r16, 0b00000010
+	sts	PCICR,r16; habilito la interrupcion
+	ldi	r16, 0b00000100
+	sts	PCMSK1,r16; habilito la interrupt del portC
+
+
+
+;Programa principal ... acá puedo hacer lo que quiero
+
+comienzo:
+	sei							;habilito las interrupciones globales(set interrupt flag)
+
+loop1:
+	nop
+	nop
+	nop
+	nop
+	ori r16, 0xFF
+	nop
+	nop
+	nop
+	brne loop1  //branch not equal 0 la zero flag es 0 salta si la operacion anterior da cero por ende va a loop 2
+loop2:
+	nop
+	nop
+	nop
+fin:
+	rjmp loop2
+
+;RUTINAS
+;-------------------------------------------------------------------------------------
+
+; ------------------------------------------------
+; Rutina de atención a la interrupción del Timer0.
+; ------------------------------------------------
+; recordar que el timer 0 fue configurado para interrumpir cada 125 ciclos (5^3), y tiene un prescaler 1024 = 2^10.
+; El reloj de I/O está configurado @ Fclk = 16.000.000 Hz = 2^10*5^6; entonces voy a interrumpir 125 veces por segundo
+; esto sale de dividir Fclk por el prescaler y el valor de OCR0A.
+; 
+; Esta rutina por ahora no hace casi nada, Ud puede ir agregando funcionalidades.
+; Por ahora solo: cambia el valor de un LED en la placa, e incrementa un contador en r24.
+
+_tmr0_int: ;interrupt del timer
+	in r17,SREG 			;este es el contexto
+	inc	r24					
+	cpi	r24,125				;comparamos
+	breq count_secs	;aca comparamos el count_secs con 125, si es 125 
+	rjmp _tmr0_out
+
+count_secs:
+	sbi PINB, 5
+	inc r18; para 2 seg
+	inc r19; para 4 seg
+	inc r20; para 8 seg
+	rjmp cambio_led ;aca vamos a empezar a cambiar los led
+
+cambio_led:
+	cpi r18, 2 ;compara si el r18 vale 2
+	breq cambio_led2 ;la instruccion anterior es igual, así que pasa a la siguiente instruccion
+	cpi r19, 4; compara, 
+	breq cambio_led3
+	cpi r20, 8
+	breq cambio_led4
+	cpi r20, 15
+	breq reiniciar_count
+	rjmp _tmr0_out
+
+cambio_led2:
+	sbi PINB, 4
+	ldi r18, 0
+	rjmp cambio_led
+
+cambio_led3:
+	sbi PINB, 3
+	ldi r19, 0
+	rjmp cambio_led
+
+cambio_led4:
+	sbi PINB, 2
+	ldi r20, 0
+	rjmp cambio_led
+
+reiniciar_count:
+	ldi r18, 0
+	ldi r19, 0
+	ldi r20, 0
+	rjmp _tmr0_out
+
+_tmr0_out:
+	out	SREG,r17			   
+	reti						;retorno de la rutina de interrupción del Timer0
+
+interrupcion_externa:
+					
+	in r17,SREG 	
+	rjmp wait
+					
+wait:
+	sbis PINC, 1
+	rjmp salir
+	sbis PINC, 3
+	rjmp reiniciar
+	rjmp wait
+								
+salir:
+	out SREG,r17
+	reti
+
+reiniciar:
+	ldi r18, 0
+	ldi r19, 0
+	ldi r20, 0
+	ldi r16, 0b00111100
+	out PORTB, r16
+	rjmp salir
+	
